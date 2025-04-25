d03e3e0e272039a110c1dc07158ed137347e4212
devanshivarshney <devanshivarshney2@gmail.com>
2025-04-26 03:04:35 +0530

    adding

---
+++ pages/header.php
@@ -0,0 +1,13 @@
+<!-- header.php -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+SC">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Crimson+Pro">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lavishly+Yours&display=swap">
+    <link rel="stylesheet" type="text/css" href="../css/allpages.css" />
+    <title>Pawsies🐾 - Homepage</title>
+</head>
